d38952fdb7c715910bca7f2fb0b84aabd3621269
Toan Ngo <ngoquoctoan9591@gmail.com>
2015-02-10 00:11:40 -0600

    initial commit

--- hotpot_app/__init__.py
+++ hotpot_app/__init__.py
@@ -3,12 +3,12 @@ from flask import Flask
 app = Flask(__name__, instance_relative_config=True)
 
 # Load dev config (default)
-app.config.from_object('config.default')
+app.config.from_object('config.dev')
 
 # Load config from instance folder
 app.config.from_pyfile('config.py')
 
 # Load file specified by APP_CONFIG
-app.config.from_envvar('APP_CONFIG_FILE')
+app.config.from_envvar('APP_CONFIG')
 
 print(app.config['SQLALCHEMY_DATABASE_URI'])
