8e070c3e08e5562224a86e451113a4be657eb6e7
Toan Ngo <ngoquoctoan9591@gmail.com>
2015-02-10 01:58:47 -0600

    added models

--- hotpot_app/__init__.py
+++ hotpot_app/__init__.py
@@ -1,4 +1,21 @@
-from .app import app
+__author__ = 'toanngo'
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, instance_relative_config=True)
+
+# Load dev config (default)
+app.config.from_object('config.default')
+
+# Load config from instance folder
+app.config.from_pyfile('config.py')
+
+# Load file specified by APP_CONFIG
+app.config.from_envvar('APP_CONFIG')
+
+
+# Register db
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run()
