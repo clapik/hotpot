8288d74d1495564df350fa342d67540248ce7e4d
Toan Ngo <ngoquoctoan9591@gmail.com>
2015-02-16 14:39:49 -0600

    deployment fixes

--- manage.py
+++ manage.py
@@ -2,13 +2,13 @@ __author__ = 'toanngo'
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 from app import app, db
-import os
 
-app.config.from_pyfile(os.environ['APP_CONFIG'])
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+print(MigrateCommand.directory)
 
 # manager.add_command('app', app.run)
 
